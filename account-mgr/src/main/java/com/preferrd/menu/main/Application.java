8b1129058287a727e8d6ec5f5fdf07231dd7bb5c
Li GuoYao <gyli@utstar.com>
2019-03-11 11:30:00 +0800

    项目启动成功

--- account-mgr/src/main/java/com/preferrd/menu/main/Application.java
+++ account-mgr/src/main/java/com/preferrd/menu/main/Application.java
@@ -1,24 +1,41 @@
 package com.preferrd.menu.main;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
-@MapperScan(basePackages = "tk.mybatis.springboot.mapper")
-public class Application extends WebMvcConfigurerAdapter {
+//@MapperScan(basePackages = "tk.mybatis.springboot.mapper")
+@RestController
+@RequestMapping(value = "/rest")
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/")
-    String home() {
-        return "redirect:cities";
+    @GetMapping("/home1")
+    ModelAndView home1() {
+        ModelAndView mv = new ModelAndView();
+        // request url不变
+        mv.setViewName("forward:/rest/cities");
+        return mv;
+    }
+
+    @GetMapping("/home2")
+    ModelAndView home2() {
+        ModelAndView mv = new ModelAndView();
+        // request url改变
+        mv.setViewName("redirect:/rest/cities");
+        return mv;
+    }
+
+    @GetMapping("/cities")
+    String cities() {
+        return "cities";
     }
 }
