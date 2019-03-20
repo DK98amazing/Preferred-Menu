0855c8c63f65d525d79acba5d6a015f8e8c67382
DK98amazing <gyli@utstar.com>
2019-03-20 20:53:25 +0800

    业务请求

--- account-mgr/src/main/java/com/preferrd/menu/account/Application.java
+++ account-mgr/src/main/java/com/preferrd/menu/account/Application.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.web.servlet.ModelAndView;
 
 
-
-@SpringBootApplication @MapperScan("com.preferrd.menu.database.dao") public class Application {
+@SpringBootApplication
+@MapperScan("com.preferrd.menu.database.dao")
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
