196bef56d24804a42770ade25bd0ea9dc6e6c757
liguoyao <liguoyao@utry.cn>
2019-08-29 22:24:17 +0800

    profile

---
+++ account-mgr/src/main/java/com/preferrd/menu/account/service/MyRequestMappingHandlerAdapter.java
@@ -0,0 +1,11 @@
+package com.preferrd.menu.account.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+@Configuration
+public class MyRequestMappingHandlerAdapter extends RequestMappingHandlerAdapter {
+    static {
+        System.out.println("RequestMappingHandlerAdapter init");
+    }
+}
