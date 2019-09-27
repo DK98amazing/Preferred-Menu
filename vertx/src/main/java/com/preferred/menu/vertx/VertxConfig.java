c50324ea29c08a4c9ab63ecc47bddcad1bf38bf8
liguoyao <liguoyao@utry.cn>
2019-09-27 16:30:59 +0800

    vert.x 分布式调通  负载均衡待测试

--- vertx/src/main/java/com/preferred/menu/vertx/VertxConfig.java
+++ vertx/src/main/java/com/preferred/menu/vertx/VertxConfig.java
@@ -1,26 +1,68 @@
 package com.preferred.menu.vertx;
 
-import com.hazelcast.config.Config;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.EventBusOptions;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.hazelcast.ConfigUtil;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import io.vertx.spi.cluster.zookeeper.ZookeeperClusterManager;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.SettableListenableFuture;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.function.Consumer;
 
 @Configuration
 public class VertxConfig {
+
+    private CuratorFramework curatorFramework;
+    private JsonObject zkConfig = new JsonObject();
+
+    @PostConstruct
+    public void init() {
+//        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+//        curatorFramework =
+//                CuratorFrameworkFactory.builder()
+//                        .connectString("127.0.0.1:2181")
+//                        .sessionTimeoutMs(60000)
+//                        .connectionTimeoutMs(5000)
+//                        .retryPolicy(retryPolicy)
+//                        /*.aclProvider(new ACLProvider() {
+//                            private List<ACL> acl ;
+//                            @Override
+//                            public List<ACL> getDefaultAcl() {
+//                                if(acl ==null) {
+//                                    ArrayList<ACL> acl = ZooDefs.Ids.CREATOR_ALL_ACL;
+//                                    acl.clear();
+//                                    acl.add(new ACL(ZooDefs.Perms.ALL, new Id("auth", "admin:admin")));
+//                                    this.acl = acl;
+//                                }
+//                                return acl;
+//                            }
+//                            @Override
+//                            public List<ACL> getAclForPath(String s) {
+//                                return acl;
+//                            }
+//                        })*/
+////                        .namespace(namespace)
+//                        .build();
+//        curatorFramework.start();
+        //        client.getConnectionStateListenable().addListener(new ZookeeperConnectionListener(host,maxRetry,sessionTimeout,connectTimeout,namespace))
+        zkConfig.put("zookeeperHosts", "10.0.7.173");
+        zkConfig.put("rootPath", "io.vertx");
+        zkConfig.put("retry", new JsonObject()
+                .put("initialSleepTime", 3000)
+                .put("maxTimes", 3).put("intervalTimes", 1000));
+    }
 
     @Bean(value = "singleVertx")
     public Vertx vertxSingle() {
@@ -33,18 +75,14 @@ public class VertxConfig {
     @Bean(value = "clusterVertx")
     public Vertx vertxCluster() {
         SettableListenableFuture<Vertx> settableListenableFuture = new SettableListenableFuture<>();
-        Config hazelcastConfig = ConfigUtil.loadConfig();
-        hazelcastConfig.getGroupConfig()
-                .setName("myClusterName");
-        ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
-        EventBusOptions eventBusOptions = new EventBusOptions().setClustered(true).setClusterPublicHost("127.0.0.1")
-                .setClusterPublicPort(6666);
+        ClusterManager mgr = new ZookeeperClusterManager(zkConfig);
+        EventBusOptions eventBusOptions = new EventBusOptions().setClustered(true);
         VertxOptions vertxOptions = new VertxOptions().setClusterManager(mgr).setEventBusOptions(eventBusOptions);
         Vertx.clusteredVertx(vertxOptions, vertxAsyncResult -> {
             if (vertxAsyncResult.succeeded()) {
                 settableListenableFuture.set(vertxAsyncResult.result());
             } else {
-                settableListenableFuture.setException(new RuntimeException("Vert.x init failed"));
+                settableListenableFuture.setException(vertxAsyncResult.cause());
             }
         });
         try {
